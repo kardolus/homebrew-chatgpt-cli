d211c4423e26557b6647d8328b6eb102d08bb1d6
kardolus <kardolus@gmail.com>
2023-09-15 20:12:09 -0400

    Bump version

--- HomebrewFormula/chatgpt-cli.rb
+++ HomebrewFormula/chatgpt-cli.rb
@@ -1,14 +1,14 @@
 class ChatgptCli < Formula
   desc "A CLI for the OpenAI ChatGPT API"
   homepage "https://github.com/kardolus/chatgpt-cli"
-  version "1.3.1"
+  version "1.3.2"
 
   if Hardware::CPU.intel?
     url "https://github.com/kardolus/chatgpt-cli/releases/download/v#{version}/chatgpt-darwin-amd64"
-    sha256 "e3ffeb7a791503eeb34188855ac37c4f3d1551457f37b2fcc0b22f7d4611030b"
+    sha256 "72e6b8951a1b791392adb5fd73f38f4e041394aea13f5ef3b77ffe046600683e"
   elsif Hardware::CPU.arm?
     url "https://github.com/kardolus/chatgpt-cli/releases/download/v#{version}/chatgpt-darwin-arm64"
-    sha256 "2ae21a064b480b2f774400dd0df8e9f8c44d8d70e3e1398fe6d1492dac984907"
+    sha256 "dfcfa8a350e6444cab4e7a52b48d88d3bb94d26f1bd4a82dafce1222479a5c82"
   end
 
   def install
